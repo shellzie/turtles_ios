065bcf5be675b24b202b821ced5add7941d757c2
dev <michellekam@gmail.com>
2016-11-09 21:27:20 -0700

    completed validations of email, password. sign in functinoality is working. if user enters invalid email or password, they are prompted with an alert window. if either field is empty they see an alert window. if record is not found in DB, then they are alerted their credentials are wrong

--- RobotPets/LoginViewController.swift
+++ RobotPets/LoginViewController.swift
@@ -24,31 +24,49 @@ class LoginViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
+    private func isValidEmail(str:String) -> Bool {
+        let emailRegEx = "[A-Z0-9a-z.]+@[A-Za-z0-9.]+\\.[A-Za-z]{2,64}"
+        let emailTest = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
+        let res = emailTest.evaluate(with: str)
+        return res
+    }
+    
+    private func isValidPassword(str:String) -> Bool {
+        let trimmed = str.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
+        let length = trimmed.characters.count
+        return (length >= 6)
+    }
+    
+    private func createAlertWindow(titleStr:String, msgStr:String) {
+        let alert = UIAlertController(title: titleStr, message:msgStr, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default) { _ in })
+        self.present(alert, animated: true){}
+    }
+    
     @IBAction func loginTapped(_ sender: UIButton) {
         let email:String = self.email.text!
         let password:String = self.password.text!
-        
-        if (email.isEmpty || password.isEmpty) {
-            let alert = UIAlertController(title: "Sign In Failed!", message:"Please enter email and password", preferredStyle: .alert)
-            alert.addAction(UIAlertAction(title: "OK", style: .default) { _ in })
-            self.present(alert, animated: true){}
+        let trimmedEmail = email.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
+        if (trimmedEmail.isEmpty || password.isEmpty) {
+            self.createAlertWindow(titleStr:"Sign In Failed!", msgStr:"Please enter email and password")
+        } else if (!self.isValidEmail(str: email)) {
+            self.createAlertWindow(titleStr:"Error with Email!", msgStr:"Please enter a valid email address")
+        } else if !self.isValidPassword(str: password) {
+            self.createAlertWindow(titleStr:"Error with password!", msgStr:"Please enter a password containing at least 6 characters")
         } else {
             let components = NSURLComponents(string: API.herokuURLString)
-            
             let paramString = "email=\(email)&password=\(password)"
             let url = components?.url
             let request = NSMutableURLRequest(url: url! as URL)
             request.httpMethod = "POST"
             request.httpBody = paramString.data(using: String.Encoding.utf8);
+            
             let task = session.dataTask(with: request as URLRequest) { (data, response, error) -> () in
                 print("++++++++++++++++++ Response is \(response) ")
                 print("++++++++++++++++++ Error is \(error) ")
                 print("++++++++++++++++++ Data is \(data) ")
                 let resp = response as! HTTPURLResponse
-                
                 if (resp.statusCode >= 200 && resp.statusCode < 300) {
-                    
                     let prefs:UserDefaults = UserDefaults.standard
                     prefs.set(email, forKey: "EMAIL")
                     prefs.set(1, forKey: "ISLOGGEDIN")
@@ -56,12 +74,22 @@ class LoginViewController: UIViewController {
                     self.dismiss(animated: true, completion: nil)
                 }
                 else {
-                    let alert = UIAlertController(title: "Log In Failed!", message:"Credentials not found", preferredStyle: .alert)
-                    alert.addAction(UIAlertAction(title: "OK", style: .default) { _ in })
-                    self.present(alert, animated: true){}
+                    
+                    DispatchQueue.main.async(execute: {
+                        self.createAlertWindow(titleStr:"Sign in Failed", msgStr:"Credentials not found")
+                    })
+                    
                 }
             }
             task.resume()
+            
+
+            
+            
+            
+                    
+            
+            
         }
     }
 }
