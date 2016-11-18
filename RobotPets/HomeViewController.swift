670392a3358e8bcc45d7a4de822c066e9664ac38
dev <michellekam@gmail.com>
2016-11-18 00:10:43 -0700

    modified the conditional checks in homeVC to support promo code page.

--- RobotPets/HomeViewController.swift
+++ RobotPets/HomeViewController.swift
@@ -24,17 +24,25 @@ class HomeViewController: UIViewController {
         super.viewDidAppear(true)
         
         let prefs:UserDefaults = UserDefaults.init()
-        if (prefs.value(forKey: "ISLOGGEDIN") == nil) {
-            print("no value set in preferences")
+//        if (prefs.value(forKey: "ISLOGGEDIN") == nil) {
+//            print("no value set in preferences")
+        
+//        }
+//        else {
+        
+        let isLoggedIn:Int = prefs.integer(forKey: "ISLOGGEDIN")
+        
+        if prefs.string(forKey: "PROMO") == nil {
+            self.performSegue(withIdentifier: "goto_promo", sender: self)
         }
-        else {
-            let isLoggedIn:Int = prefs.integer(forKey: "ISLOGGEDIN")
-            if (isLoggedIn != 1) {
-                self.performSegue(withIdentifier: "goto_login", sender: self)
-            } else {
-                self.usernameLabel.text = prefs.string(forKey: "EMAIL") 
-            }
+        else if (isLoggedIn != 1) {
+            self.performSegue(withIdentifier: "goto_register", sender: self)
+        } else {
+            self.usernameLabel.text = prefs.string(forKey: "EMAIL")
         }
+//        else {
+//            print("error eoccured. no promo stored in prefs")
+//        }
     }
     
     @IBAction func logoutTapped(_ sender: UIButton) {
